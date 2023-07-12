e6ac936ed6bd7ad60e9880a076ec605cf72300e1
dwinugrohoaqil <aqilnugroho.an@gmail.com>
2023-07-12 08:34:15 +0700

    kasih nama dan addressingn kv

--- mainx_infrared.py
+++ mainx_infrared.py
@@ -79,8 +79,7 @@ class SecondWindow(Screen,BoxLayout):
                 return print(databases)        
         except Error as e:      
             print("Error ", e)
-        
-    
+          
 
 class ThirdWindow(Screen):
 
@@ -195,10 +194,10 @@ class FourWindow(Screen):
 class WindowManager(ScreenManager):
     pass
 
-kv = Builder.load_file("guidb.kv")
+kv = Builder.load_file("mine_infrared.kv")
     
 class MyMainApp(App):
-    title = "Tugas Kelompok AqilBimaRioAmel"
+    title = "IMSql"
     def build(self):
         return kv
     
